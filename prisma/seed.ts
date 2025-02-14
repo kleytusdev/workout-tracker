7041dc8152fabe4e64a2eafa6afc164e7e63b209
Aldo de la Cruz <pedroaldo14@gmail.com>
2025-02-13 21:47:10 -0500

    feat: :sparkles: Creating migration and seeder with Prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: { },
+    create: {
+        email: 'alice@prisma.io',
+        username: 'Alice',
+      }
+    })
+
+  await prisma.user.create({
+    data: {
+      email: 'bob@prisma.io',
+      username: 'Bob',
+    }
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
