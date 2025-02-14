7c7877095e9b1509e13dd15f1f266bbaac26b402
Aldo de la Cruz <pedroaldo14@gmail.com>
2025-02-13 23:05:17 -0500

    feat: :construction: Creating Prisma service and module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { SignupDto } from './dtos';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   async signup(payload: SignupDto) {}
 }
