398cf1418e3b0f94c047b708473286b024bff318
Aldo de la Cruz <pedroaldo14@gmail.com>
2025-02-14 20:02:06 -0500

    feat: :safety_vest: Validate if user exists before signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { SignupDto } from './dtos';
 import { AuthService } from './auth.service';
+import { UniqueUserPipe } from './validators/unique-user.validator';
 
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
+  @UsePipes(UniqueUserPipe)
   async signup(@Body() signupDto: SignupDto) {
+    console.log(signupDto);
     return await this.authService.signup(signupDto);
   }
 }
--- src/auth/dtos/signup.dto.ts
+++ src/auth/dtos/signup.dto.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class SignupDto {
   @IsEmail()
